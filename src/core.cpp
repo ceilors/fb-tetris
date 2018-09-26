2c54c83449939ce346bb6e876c165e8df84dd568
Alexey Golubev <FreeCX@users.noreply.github.com>
2018-09-26 11:30:48 +0300

    fix figure placing bug

--- src/core.cpp
+++ src/core.cpp
@@ -192,6 +192,7 @@ void Tetris::move(uint8_t state, bool delay) {
             if (field.intersect(&curr)) {
                 curr.pos.x++;
             }
+            set_flag = false;
             break;
         }
         case MOVE_RIGHT: {
@@ -199,6 +200,7 @@ void Tetris::move(uint8_t state, bool delay) {
             if (field.intersect(&curr)) {
                 curr.pos.x--;
             }
+            set_flag = false;
             break;
         }
         case MOVE_SOFT_DOWN: {
@@ -229,6 +231,7 @@ void Tetris::move(uint8_t state, bool delay) {
         case ROTATE_RIGHT: {
             bool side = true ? state == ROTATE_LEFT : false;
             bool ignored_all = true;
+            set_flag = false;
             curr.rotate(side);
             if (field.intersect(&curr)) {
                 for (int8_t k = 1; k <= std::max(curr.x_max, curr.y_max); ++k) {
